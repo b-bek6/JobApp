1ef93f42eb79f111b7f541e4f9e93ce78b610043
Bibek Ghimire <89078753+b-bek6@users.noreply.github.com>
2023-05-09 14:19:53 +0545

    profile image feature update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
+import { AiOutlineSearch } from "react-icons/ai"
+import Link from "next/link"
 export default function Navbar(){
     return (
         <>
-            <header className="bg-[#EEEFFB] flex items-center justify-around h-[78px]">
+            <header className="p-2 gap-2 bg-[#EEEFFB] flex items-center justify-around h-[78px] md:flex-col md:container md:h-full">
                 <div>
-                    <logo className="font-sans text-[#0D0E43] ">Heko</logo>
+                    <logo href={"/"} className='text-4xl font-bold text-[#0D0E43]'>Heko</logo>
                 </div>
                 <div>
                     <nav>
@@ -17,10 +19,12 @@ export default function Navbar(){
                         </ul>
                     </nav>
                 </div>
-                <div>
-                    <input type="text"></input>
-                    <logo><button>search</button></logo>
-                </div>
+                <form className="flex mt-3">
+                    <input type="text" className="border w-full" />
+                    <logo className="bg-secondary text-white p-2 inline">
+                        <AiOutlineSearch className="inline"/>
+                    </logo>
+                </form>
             </header>
         </>
     )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -13,7 +13,8 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors:{
-        primary: "#7E33E0"
+        primary: "#7E33E0",
+        secondary: "#FB2E86"
       }
     },
   },
