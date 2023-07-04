c3d3f3cc381d6dd37d32d89b0678b3925a0e7ce2
Bibek Ghimire <89078753+b-bek6@users.noreply.github.com>
2023-07-04 20:55:56 +0545

    env delete

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://JobApp:jobapp@cluster0.dkoidso.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
--- server/middleware/checkAuthentication.js
+++ server/middleware/checkAuthentication.js
@@ -7,8 +7,7 @@ const checkAuthentication = (req, res, next) => {
         // console.log(token);
         if(token){
             try {
-                // var decoded_user_info = jwt.verify(token, process.env.JWT_SECRET);
-                var decoded_user_info = jwt.verify(token, shhhhh);
+                var decoded_user_info = jwt.verify(token, process.env.JWT_SECRET);
                 req.user = decoded_user_info;
                 return next()
             } catch (error) {
