dfcc2a1fae77f5fc604fadac201157bb9d038528
Bibek Ghimire <89078753+b-bek6@users.noreply.github.com>
2023-07-06 10:57:33 +0545

    cors

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.static('uploads'));
 
 // MIDDLEWARE
 app.use(express.json()); // req.body (change incomming req to the json format)
-app.use(cors);
+app.use(cors());
 
 
 app.use((req, res, next) => {
