7c586a621cfb5a7694649ce6ce5f125307ef54b5
Bibek Ghimire <89078753+b-bek6@users.noreply.github.com>
2023-07-11 23:05:18 +0545

    vercel

--- client/app/jobs/page.jsx
+++ client/app/jobs/page.jsx
@@ -3,19 +3,44 @@ import { useEffect, useState } from 'react';
 import Job from '@components/Job';
 import axios from 'axios';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 
-export default function page() {
+export default function page(ctx) {
+  console.log(ctx)
+  const router = useRouter()
   const [jobs, setJobs] = useState([])
+  let get
   useEffect(()=>{
-    axios.get('http://localhost:8001/api/jobs').then(response => {
+    axios.get(`http://localhost:8001/api/jobs?search_term=${ctx?.searchParams?.search_term}`).then(response => {
       setJobs(response.data.data[0].jobs);
   });
   },[]);
+
+    function handleSearch (e) {
+      e.preventDefault()
+      router.push(`/jobs?search_term=${e.target.search_term.value}`)
+    }
   
   return (
     <div>
         <div className='bg-secondary p-6 text-2xl font-Poppins flex justify-center'>All Popular Listed Jobs</div>
+        <div className='border bg-secondary p-6 flex justify-around'>
+        <div className='flex gap-2'>
+                    <label htmlFor="type" className='text-lg  text-light-primary'>Per Page</label>
+                    <select  className=' border-2 rounded-md text-light-primary'name='type'>
+                      <option selected value="5">5</option>
+                      <option value="10">10</option>
+                      <option value="15">15</option>
+                    </select>
+          </div>
+          <form onSubmit={handleSearch}>
+              <span><input type="text" name='search_term' className='border outline-none p-1.5 px-3 font-sans' /></span>
+              <button type='submit' className='border rounded-md font-bolt bg-primary text-white p-1.5'>
+                Search
+              </button>
+            </form>
+        </div>
         <div className=' container grid'>
         
       {
@@ -33,3 +58,7 @@ export default function page() {
     </div>
   )
 }
+
+// export async function getServerSideProps(ctx){
+//   let res = await axios.get('http://localhost:8001/api/jobs')
+// }
--- client/components/Job.jsx
+++ client/components/Job.jsx
@@ -6,14 +6,13 @@ export default function Job({job}) {
   const redux_user = useSelector(store=> store.user.value)
   return (
     <div className='container border m-3'>
-        <div className='flex flex-col align-middle items-center justify-center md:flex-row gap-2 md:gap-10 m-3'>
+        <div className='flex flex-col align-middle justify-center items-center md:flex-row gap-2 md:gap-10 m-3'>
             <div className='rounded-full hrounded-full w-20 h-20 bg-primary' >
-                
             </div>
-            <div className='flex flex-col justify-center items-center md:items-start '>
+            <div className='flex flex-col justify-center w-1/2 align-middle md:items-start '>
                 <div className='font-Poppins text-1xl'>{`${job.company_name}`} </div>
                 <div className='font-bold text-xl'>{`${job.name} (${job.joblevel})`}</div>
-                <div className='grid grid-cols-2 md:grid-cols-3 gap-1'>
+                <div className='grid grid-cols-2 md:flex gap-1'>
                     <div className='col-span-2 md:col-span-1'>{`${job.location}`}</div>
                     <div>{`${job.category},`}</div>
                     <div>Rs {job.offered_salary}</div>
